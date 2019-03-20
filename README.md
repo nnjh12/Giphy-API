8daa430addbcb58e2d61c42f65ac7ddad0073525
Jinhwa Roh <nnjh12@gmail.com>
2019-03-19 23:44:56 -0400

    add style

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
-# Giphy-API
+# Week 06 Homework. **Giphy AIP**
+## What I learned in each process
+1. Create buttons with for loops
+    * Empty container first. If not, the buttons will be duplicated.
+    * Give the button-value attributes, to use it later when each button is clicked.
+1. On click event 1. **Click add button**
+    * Make submit button with `<input>` instead of `<button>` to make Enter key work.
+    * `event.preventDefault()` to prevent Default behavior of `<input type=submit>` which is refreshing the page.
+    * When user click submit button, empty what user typed. `.empty()` is not working. Use `.val("")`.
+1. On click event 2. **Click character button** (Call the images from Giphy API with AJAX Call when user click character buttons.)
+    * Empty container first
+    * Get the info of what user clicked: `$(this).attr("button-value")`
+    * Create the URL including the info of what user clicked.
+    * Log the response to see JSON object in my console to pick the needed key.
+    * Grab the needed information by using object key and update it into HTML.
+    * *In this project, give attributes of still & animate image URL to each images.*
+1. On click event 3. **Click image**
+    * Use the attributes to change image to still from animate.
+    * Update `<img src=>` to still image URL or animate image URL which were assigned in attributes from previous step.
+1. On click event 4. **Click close button**
+    * Bootstrap close button. (It is not working without JS)
+    * Need to write separate JS as below to make it work.
+   ```javascript
+   $('.close').on('click',function() {
+    $(this).closest('.card').fadeOut()})
+    ```
+
+
+## Small Tips
+* There are many buttons with different roles, so use ID or Class name for each button's On click event. 
+* Bootstrap card with class card-coloums `<div class="card-columns">` make each card stacks with out space.
+  (Cards can be organized into Masonry-like columns. Cards are ordered from top to bottom and left to right.)
--- assets/app.js
+++ assets/app.js
@@ -44,11 +44,16 @@ $("#add-button").on("click", function (event) {
         createButton()
     }
 
+    // empty user-type div
+    $("#user-type").val("")
 })
 
 
 ///// STEP4. when user click character button (ajax call)
-$(document).on("click","button", function () {
+$(document).on("click", ".character-button", function () {
+    // empty image-container
+    $("#image-container").empty()
+
     // get the value of the button which user clicked
     var userClicked = $(this).attr("button-value")
     console.log(userClicked)
@@ -65,28 +70,34 @@ $(document).on("click","button", function () {
 
         for (var i = 0; i < 10; i++) {
             // from the JSON object, find the url of still image in fixed height
-            var stillImageUrl = response.data[i].images.fixed_height_still.url
-            var animateImageUrl = response.data[i].images.fixed_height.url
+            var stillImageUrl = response.data[i].images.fixed_width_still.url
+            var animateImageUrl = response.data[i].images.fixed_width.url
 
+            // to use the bootstrap card format, create new card div
+            var card = $("<div>").attr("class", "card border-secondary mb-3")
+            var cardBody = $("<div>").attr("class", "card-body").html("<p>" + "Rating: " + response.data[i].rating + "<p>")
 
             // create new image tag and add src
             var imageTag = $("<img>").attr("src", stillImageUrl)
+            imageTag.attr("class", "card-img-top")
+            imageTag.attr("alt", userClicked)
 
             // add status attributes to stop and animate the images
             imageTag.attr("status", "still")
 
             // add stillImageUrl & animateImageUrl for future use
-            imageTag.attr("still-image-url",stillImageUrl)
-            imageTag.attr("animate-image-url",animateImageUrl)
+            imageTag.attr("still-image-url", stillImageUrl)
+            imageTag.attr("animate-image-url", animateImageUrl)
 
             // add bootstrap class to have margins 
-            imageTag.attr("class","mb-2 mr-2")
+            imageTag.attr("class", "mb-2 mr-2")
 
 
             // append image in image container
-            $("#image-container").prepend(imageTag)
+            card.prepend(cardBody)
+            card.prepend(imageTag)
+            $("#image-container").prepend(card)
         }
-
     })
 })
 
@@ -104,18 +115,23 @@ $(document).on("click", "img", function () {
     // if image is still, 
     if (imageStatus === "still") {
         // change the status to animate
-        $(this).attr("status","animate")
+        $(this).attr("status", "animate")
 
         // and update the src to animate src
-        $(this).attr("src",animateImageSrc)
+        $(this).attr("src", animateImageSrc)
     }
 
     // if image is animate
     if (imageStatus === "animate") {
         // change the status to still
-        $(this).attr("status","still")
+        $(this).attr("status", "still")
 
         // and update the src to still src
-        $(this).attr("src",stillImageSrc)
+        $(this).attr("src", stillImageSrc)
     }
+})
+
+///// STEP6. close instruction card
+$('.close').on('click', function () {
+    $(this).closest('.card').fadeOut();
 })
